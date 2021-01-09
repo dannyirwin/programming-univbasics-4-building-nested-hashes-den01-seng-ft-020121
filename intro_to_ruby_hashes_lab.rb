08998e37ac91236142713c36aef4268a90f3241b
Daniel Irwin <dirwin123@gmail.com>
2021-01-09 19:30:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   {railroads: {
     pieces: 4,
-    rent_int_dollars: {},
+    rent_in_dollars: {},
     names: {}
   }}
 
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   # complexity.
   {railroads: {
     pieces: 4,
-    rent_int_dollars: {
+    rent_in_dollars: {
       one_piece_owned: 25,
       two_pieces_owned: 50,
       three_pieces_owned: 100,
